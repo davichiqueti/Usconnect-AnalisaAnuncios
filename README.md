5a99a985f8fa972196ce591aa539a6e0ce60ef7c
davichiqueti <davichiqueti@gmail.com>
2023-08-11 01:34:28 -0300

    updated code

--- README.md
+++ README.md
@@ -1,51 +1,53 @@
 # Usconnect-AnalisaAnuncios
 
-   Sistema projetado para extrair informações do Mercado Livre atraves de sua API oficial. A busca pode ser feita pelo nome do produto ou do vendedor. Os dados são tratados e transferidos para uma tabela do Excel.
+Sistema projetado para extrair informações do Mercado Livre atraves de sua API oficial. A busca pode ser feita pelo nome do produto ou do vendedor. Os dados são tratados e transferidos para uma tabela do Excel.
 
-   Projeto exigido pelo E-commerce em que atuo. Logo, foi meu primeiro projeto profissional. É muito gratificante poder aplicar meus conhecimentos na resolução de problemas da minha equipe!
+Projeto exigido pelo E-commerce em que atuo. Logo, foi meu primeiro projeto profissional. É muito gratificante poder aplicar meus conhecimentos na resolução de problemas da minha equipe!
 
 
 ## Objetivo:
 
-   Automatizar o processo da coleta de dados do Mercado Livre para Gerar Insights e estratégias de marketing com informações coletadas. Permitindo as seguintes análises:
+Automatizar o processo da coleta de dados do Mercado Livre para Gerar Insights e estratégias de marketing com informações coletadas. Permitindo as seguintes análises:
 
-   * Avaliar rentabilidade de novos produtos.
-   * Encontrar os melhores métodos utilizados para anunciar um produto.
-   * Analisar anúncios de um concorrente.
-   * Visão geral sobre o rendimento de anúncios em nossas contas no MarketPlace.
+* Avaliar rentabilidade de novos produtos.
+* Encontrar os melhores métodos utilizados para anunciar um produto.
+* Analisar anúncios de um concorrente.
+* Visão geral sobre o rendimento de anúncios em nossas contas no MarketPlace.
    
 ## Tecnologias Utilizadas:
 
-   * Estruturação e execução de *Requisições HTTP* utilizando *Requests*
-   * Tratamento de dados com *Pandas*
-   * Passagem de parâmetros por linha de comando utilizando *argparse*
+* Estruturação e execução de *Requisições HTTP* utilizando *Requests*
+* Tratamento de dados com *Pandas*
+* Passagem de parâmetros por linha de comando utilizando *argparse*
 
 ## Exemplo de saída:
 
-   ![Exemplo.png](https://github.com/davichiqueti/Usconnect-AnalisaAnuncios/blob/main/Exemplo.png)
-   > apenas 5 itens para visualização
+![Exemplo.png](https://github.com/davichiqueti/Usconnect-AnalisaAnuncios/blob/main/Exemplo.png)
+> apenas 5 itens para visualização
    
 ## Como Usar:
 
-   __1 -__ Baixar interpretador [Python](https://www.python.org/downloads/)
+1. Baixar interpretador [Python](https://www.python.org/downloads/)
 
-   __2 -__ Instalar Bibliotecas especificadas em [requirement.txt](https://github.com/davichiqueti/Usconnect-AnalisaAnuncios/blob/main/requirements.txt)
+2. Instalar Bibliotecas especificadas em [requirement.txt](https://github.com/davichiqueti/Usconnect-AnalisaAnuncios/blob/main/requirements.txt)
 
-   __3 -__ Fazer o dowload do código python no diretório de destino das tabelas
+3. Fazer o dowload do código python no diretório de destino das tabelas
 
-   __4 -__ Abrir o prompt de comando e rodar comando ```cd C:\"CaminhoDoDiretório"**```
+4. Abrir o prompt de comando e rodar comando ```cd C:\"CaminhoDoDiretório"**```
 
-   __5 -__ Rodar comando ```python usco-AnalisaAnuncios-v1.1.py -v "nome do vendedor"```
+5. Rodar comando ```python usco-AnalisaAnuncios-v1.1.py -v "nome do vendedor"```
    > Mais informações sobre os parametros com -h ou --help
 
 ## Observação:
 
-   Há um limite de 1000 anúncios para a forma atual que a API está sendo consumida.
-   Conforme as intruções passadas para mim a quantidade atual já permite muitas análises e não é interessante investir tempo para quebrar esse limite.
+Há um limite de 1000 anúncios para a forma atual que a API está sendo consumida.
+Conforme as intruções passadas para mim a quantidade atual já permite muitas análises e não é interessante investir tempo para quebrar esse limite.
+
+O código está atualmente sendo revisado. Quero implementar orientação a objetos utilizando classe para melhor organização, também percebi a repetição desnecessária pela falta de uma função única para realizar as requisições.
 
 ## Referências:
 
-   [Documentação Bibliteca Requests](https://requests.readthedocs.io/en/latest/)
+[Documentação Bibliteca Requests](https://requests.readthedocs.io/en/latest/)
 
-   [Documentação Mercado Livre API](https://developers.mercadolivre.com.br/pt_br/api-docs-pt-br)
+[Documentação Mercado Livre API](https://developers.mercadolivre.com.br/pt_br/api-docs-pt-br)
 
--- usco-AnalisaAnuncios-v1.1.py
+++ usco-AnalisaAnuncios-v1.1.py
@@ -10,7 +10,6 @@ import argparse
 
 
 def requestAdsBySeller(seller_name, sort, offset, limit, filter):
-    # Requisição para extrair os dados a partir do nome de um vendedor
     url = f"https://api.mercadolibre.com/sites/MLB/search?"
     params = {
         'nickname': seller_name,
@@ -19,35 +18,25 @@ def requestAdsBySeller(seller_name, sort, offset, limit, filter):
         'limit': limit,
     }
 
-    if filter and ('internacional' in filter):
-        # Adiciona o filtro de venda internacional pelo ID
-        params['shipping_origin'] = 10215069
-    
-    if filter and ('best_sellers' in filter): 
-        # Adiciona o filtro de melhores vendedores
-        params['power_seller'] = 'yes'
+    if filter:
+        if ('internacional' in filter):
+            params['shipping_origin'] = 10215069    # Adiciona o filtro de venda internacional pelo ID
+        elif filter and ('best_sellers' in filter): 
+            params['power_seller'] = 'yes'          # Adiciona o filtro de melhores vendedores
 
-
-    try: 
-        # Tentativa de realizar a requisição
+    try:
         response = requests.get(url, params=params)
         data = response.json()
 
         return data['results']
-    
-    except requests.exceptions.ConnectTimeout as e: 
-
+    except requests.exceptions.ConnectTimeout as e:
         print("\nErro de tempo limite de conexão:", e)
-
     except requests.exceptions.RequestException as e: 
-        
         print("\nErro de requisição:", e)
 
 
 
-
 def requestAdsByProduct(search_query, sort, offset, limit, filter):
-    # Requisição para extrair os dados a partir do nome de um produto
     url = f"https://api.mercadolibre.com/sites/MLB/search?"
     params = {
         'q' : search_query,
@@ -56,87 +45,63 @@ def requestAdsByProduct(search_query, sort, offset, limit, filter):
         'limit': limit,
     }
 
-    if filter and ('internacional' in filter):
-        # Adiciona o filtro de venda internacional pelo ID
-        params['shipping_origin'] = 10215069
-    
-    if filter and ('best_sellers' in filter): 
-        # Adiciona o filtro de melhores vendedores
-        params['power_seller'] = 'yes'
-
+    if filter:
+        if ('internacional' in filter):
+            params['shipping_origin'] = 10215069    # Adiciona o filtro de venda internacional pelo ID
+        elif filter and ('best_sellers' in filter): 
+            params['power_seller'] = 'yes'          # Adiciona o filtro de melhores vendedores
 
     try: 
-        # Tentativa de realizar a requisição
         response = requests.get(url, params=params)
         data = response.json()
 
         return data['results']
-    
     except requests.exceptions.ConnectTimeout as e: 
-
         print("\nErro de tempo limite de conexão:", e)
-
-    except requests.exceptions.RequestException as e: 
-        
+    except requests.exceptions.RequestException as e:
         print("\nErro de requisição:", e)
 
 
 
 def requestTotalItems(search_query, is_seller_search=True, filter = None):
-    # Função para extrair a quantidade total de anúncios
     params = {}
     params['limit'] = 1
 
     if is_seller_search:
-        # Extrai a quantidade de anúncios do vendedor
         url = f"https://api.mercadolibre.com/sites/MLB/search?"
         params = {'nickname': search_query}
-
     else:
-        # Extrai a quantidade de anúncios do produto
         url = f"https://api.mercadolibre.com/sites/MLB/search?"
         params = {'q' : search_query}
 
+    if filter:
+        if ('internacional' in filter):
+            params['shipping_origin'] = 10215069    # Adiciona o filtro de venda internacional pelo ID
+        elif filter and ('best_sellers' in filter): 
+            params['power_seller'] = 'yes'          # Adiciona o filtro de melhores vendedores
 
-    if filter and ('internacional' in filter):
-        # Aciona o filtro de venda internacional pelo ID
-        params['shipping_origin'] = 10215069
-    
-
-    if filter and ('best_sellers' in filter):
-        # Aciona o filtro de melhores vendedores
-        params['power_seller'] = 'yes'
-
-
-    try: 
-        # Tentativa de realizar a requisição
+    try:
         response = requests.get(url, params=params)
         data = response.json()
 
         return data['paging']['total']
-    
     except requests.exceptions.ConnectTimeout as e: 
-
         print("\nErro de tempo limite de conexão:", e)
-
-    except requests.exceptions.RequestException as e: 
-        
+    except requests.exceptions.RequestException as e:
         print("\nErro de requisição:", e)
 
 
 
 def getVisits(ad_id):
-    # Request para extrair o número de visitas de um anúncio
     url = f'https://api.mercadolibre.com/visits/items?ids={ad_id}'
     response = requests.get(url)
     data = response.json()
         
-    if response.status_code == 200: return data[ad_id]
-
-    elif response.status_code == 429: return getVisits(ad_id) # Recursividade para forçar resposta da API
-
+    if response.status_code == 200:
+        return data[ad_id]
+    elif response.status_code == 429: 
+        return getVisits(ad_id) # Recursividade para forçar resposta da API
     else:
-
         print("\nAPI ERROR:", response.status_code)
         print("Erro ao extrair as visitas:", response.text)
 
@@ -144,48 +109,46 @@ def getVisits(ad_id):
 
 def pagingLoop(search_query, sort, is_seller_search=True, filter = None):
     # Looping realizar a paginação considerando um limite de 50 anúncios por requisição
-    offset = 0 # Ponto Inicial
+    offset = 0
     limit = 50
     analysisDate = datetime.now().date().strftime("%d/%m/%Y")
     adsDataList = []
-    # Operador ternário para associar a variavel "total_items" ao retorno da função correta
-    total_items = requestTotalItems(search_query, is_seller_search=True, filter = filter) if is_seller_search else requestTotalItems(search_query, is_seller_search=False, filter = filter)
+    total_items = requestTotalItems(search_query, is_seller_search, filter = filter) 
     pbar = tqdm(total=total_items, desc='Extraindo Dados') # Cria uma barra de progresso
 
     while True:
-        # Operador ternário para associar a variavel "data" ao retorno da função correta
-        data = requestAdsBySeller(search_query, sort, offset, limit, filter) if is_seller_search else requestAdsByProduct(search_query, sort, offset, limit, filter)
+        if is_seller_search: 
+            data = requestAdsBySeller(search_query, sort, offset, limit, filter)
+        else: 
+            data = requestAdsByProduct(search_query, sort, offset, limit, filter)
 
         if not data or len(adsDataList) == total_items:
-            # Finaliza o loop caso não haja dados ou se foram extraídos todos os dados
             break
 
-        for item in data:
-            # Tratando dados de cada anúncio da resposta
-            creation_date = convertDate(item['stop_time']) # Conversão da data de criação
-            days_active = calculateDateDifference(creation_date)# Calcula a quantidade de dias desde a criação do anúncio
+        for item in data:   # Tratando dados de cada anúncio da resposta
+            creation_date = convertDate(item['stop_time']) 
+            days_active = calculateDateDifference(creation_date)
             sales = item['sold_quantity']
             sales_per_day = sales / days_active if days_active != 0 else 0 # Operador ternário para evitar divisões por 0
-            visits = getVisits(item['id']) # Chamada isolada para receber o número de visitas
-            # Associação dos dados do anúncio em um dicionario
+            visits = getVisits(item['id'])
+            
             ad = {
                 'ID' : item['id'],
                 'Título' : item['title'],
                 'Preço' : item['price'],
-                'Tipo' : 'Premium' if item['listing_type_id'] == 'gold_pro' else 'Clássico', # Tratamento de dados
+                'Tipo' : 'Premium' if item['listing_type_id'] == 'gold_pro' else 'Clássico',
                 'Vendas' : sales,
                 'Visitas' : visits,
                 'Conversão' : sales / visits if item['sold_quantity'] != 0 else 0, # Operador ternário para evitar divisões por 0
                 'Data de Criação' : creation_date,
                 'QTD Dias Ativo' : days_active,
                 'Vendas/Dias' : sales_per_day,
-                'Envio' : 'Normal' if item['shipping']['logistic_type'] != 'fulfillment' else 'Normal', # Tratamento de dados
+                'Envio' : 'Normal' if item['shipping']['logistic_type'] != 'fulfillment' else 'Normal',
                 'Link': item['permalink'],
                 'Data da Análise' : analysisDate
             }
 
             if not is_seller_search: 
-                
                 ad['Vendedor']  = item["seller"]["nickname"]
                 ad['Origem']    = item['address']['state_id']
             
@@ -198,31 +161,24 @@ def pagingLoop(search_query, sort, is_seller_search=True, filter = None):
 
 
 def saveDictsAsExcel(dict_list, filename):
-    # Salva os dados em uma tabela Excel na pasta "Dados Extraídos"
     current_directory = os.getcwd()
     target_directory = os.path.join(current_directory, "Dados Extraídos")
 
-    if not os.path.exists(target_directory): 
-        
+    if not os.path.exists(target_directory):
         os.makedirs(target_directory)
     
     full_filename = os.path.join(target_directory, f"{filename}.xlsx")
 
-    if os.path.exists(full_filename): # Tratamento para arquivos já existentes
-
+    if os.path.exists(full_filename):
         i = 2
 
         while os.path.exists(f"{filename}.xlsx"):
-
             match = re.search(r'\((\d+)\)', filename)
 
             if match: 
-
                 new_number = str(i)
                 filename = re.sub(r'\(\d+\)', f'({new_number})', filename)
-
             else: 
-                
                 filename = f'{filename} ({i})'
 
         i += 1
@@ -235,7 +191,7 @@ def saveDictsAsExcel(dict_list, filename):
 
 
 def convertDate(date):
-    # Tratamento da "stop_date" para acessar a data de criação(Informação que não é disponibilizada pela API)
+    # Tratamento da "stop_date" para encontrar a data de criação
     date_obj = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
     corrected_date = date_obj - relativedelta(years=20) + timedelta(days=5)
     formatted_date = corrected_date.strftime("%d/%m/%Y")
@@ -255,19 +211,13 @@ def calculateDateDifference(date):
 def clear():
     # Limpar o prompt
     if os.name == 'nt':
-        
         os.system('cls') # Comando Windows
-
     else: 
-        
         os.system('clear') # Comando Mac e Linux
 
 
-
 def main(args):
-
     clear()
-    # Definição dos argumentos
     parser = argparse.ArgumentParser(description='USconnect Analisa Anuncios - Este programa permite analisar anúncios por vendedor ou item.')
     parser.add_argument('-v', '--vendedor')
     parser.add_argument('-i', '--item')
@@ -283,39 +233,26 @@ def main(args):
     output_filename = args.nome_arquivo
     filter = args.filtro
 
-    if seller_name: # Chamada da função de extração na pesquisa por vendedor
-        
+    if seller_name:
         adsDataList = pagingLoop(seller_name, sort, is_seller_search=True, filter = filter)
-
-    elif item_name: # Chamada da função de extração na pesquisa por item
-        
+    elif item_name:
         adsDataList = pagingLoop(item_name, sort, is_seller_search=False, filter = filter)
-
-    else: # Tratando casos em que nenhum item ou produto é especificado
-
+    else:
         print("""
                 Você deve especificar o nome do vendedor (-v USCONNECT) ou do item (-i "Iphone 7")
             
                 Comando -h ou --help para mais informações
         """)
-
         return
 
-
     if not output_filename: 
-        # Defininindo o nome do arquivo conforme o tipo de pesquisa com operador ternário
         output_filename = f"Anuncios - {seller_name}" if seller_name else f"Anuncios - {item_name}"
 
-
     if adsDataList: 
-        
-        saveDictsAsExcel(adsDataList, output_filename) # Salvar os dados em Excel
-
+        saveDictsAsExcel(adsDataList, output_filename)
     else:
-        
         print("Nenhum dado foi encontrado. Verifique o nome do vendedor ou item") # Resposta para conjuntos vazios
 
 
 if __name__ == '__main__':
-
     main(sys.argv[1:])
